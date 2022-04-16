b2eeeccf9b49f1ac6416dd1506b784a176c7fe73
Alexis Salgado Tacuri <xel.salg@gmail.com>
2022-04-16 13:08:50 -0500

    feat: Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+const MATERIAL_MODULE = [MatToolbarModule, MatIconModule, MatButtonModule];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ...MATERIAL_MODULE
   ],
   providers: [],
   bootstrap: [AppComponent]
